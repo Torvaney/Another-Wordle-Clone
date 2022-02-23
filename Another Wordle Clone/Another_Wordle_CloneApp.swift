0bb0e53febb7dd6e837391fb779b9cb25d9e1029
Ben Torvaney <torvaney@protonmail.com>
2022-02-23 20:50:16 +0000

    Initial Commit

---
+++ Another Wordle Clone/Another_Wordle_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Another_Wordle_CloneApp.swift
+//  Another Wordle Clone
+//
+//  Created by Ben Torvaney on 23/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct Another_Wordle_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
