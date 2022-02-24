2c6f283a99e7b7e76857f91a3218d1c5dc799e9e
Ben Torvaney <torvaney@protonmail.com>
2022-02-24 12:44:57 +0000

    Add basic SwiftUI views
    So that it looks like it might work, even though it's nowhere close

--- Another Wordle Clone/ContentView.swift
+++ Another Wordle Clone/ContentView.swift
@@ -9,9 +9,76 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, Wordle!")
-            .fontWeight(.bold)
-            .padding()
+        VStack {
+            Title()
+            Guesses()
+        }.padding(.vertical)
+    }
+}
+
+struct Title: View {
+    var body: some View {
+        VStack {
+            Text("Hello, Wordle!")
+                .font(.title)
+        }
+    }
+}
+
+
+struct Guesses: View {
+    var body: some View {
+        VStack {
+            Row(letters: ["C", "A", "R", "E", "T"])
+            Row(letters: ["I", "N", "D", "O", "L"])
+            Row()
+            Row()
+            Row()
+            Row()
+        }.padding(.horizontal)
+    }
+}
+
+
+struct Row: View {
+    var letters: [Character?] = [nil, nil, nil, nil, nil]
+    
+    var body: some View {
+        HStack {
+            ForEach(letters, id: \.self) { letter in
+                LetterCard(letter: letter)
+            }
+        }
+    }
+}
+
+
+struct LetterCard: View {
+    // Note: Will need a more complex data type to represent each letter card in future.
+    // Letters can be
+    //   * empty
+    //   * guessed (temp),
+    //   * guessed (submitted)
+    //   * guessed (submitted, in word)
+    //   * guessed (submitted, in right place)
+    var letter: Character?
+    
+    var body: some View {
+        let outline = RoundedRectangle(cornerRadius: 15)
+            .stroke(lineWidth: 2)
+        
+        switch letter {
+        case .some(let char):
+            ZStack {
+                outline
+                Text(String(char))
+                    .bold()
+            }
+        case .none:
+            ZStack {
+                outline
+            }
+        }
     }
 }
 
@@ -22,7 +89,12 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+                .preferredColorScheme(.light)
+            ContentView()
+                .preferredColorScheme(.dark)
+        }
     }
 }
 
