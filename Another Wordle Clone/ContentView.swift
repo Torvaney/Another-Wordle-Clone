22a0db12f3c11e2a34afe675f851aca9a1baeb89
Ben Torvaney <torvaney@protonmail.com>
2022-02-24 11:37:28 +0000

    Hello Wordle!

--- Another Wordle Clone/ContentView.swift
+++ Another Wordle Clone/ContentView.swift
@@ -9,13 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        Text("Hello, Wordle!")
+            .fontWeight(.bold)
             .padding()
     }
 }
+
+
+
+
+// Previews
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
